f19794ffeef0490ac359e6894a659cbd9e996556
kalavalli <kalavalli.s99@gmail.com>
2022-12-24 15:55:50 +0530

    doneeee

--- src/components/header/Navigation.jsx
+++ src/components/header/Navigation.jsx
@@ -9,11 +9,11 @@ const Navigation = () =>{
         <div>
             <Navbar bg="dark" expand="lg">
       <Container>
-        <Navbar.Brand><Link to="/" className='mainmenu'><h1>RESTAURANT</h1></Link></Navbar.Brand>
+        <Navbar.Brand><Link to="/home" className='mainmenu'><h1>RESTAURANT</h1></Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link> <Link to="/" className='mainmenu'>Home</Link></Nav.Link>
+            <Nav.Link> <Link to="/menu" className='mainmenu'>Home</Link></Nav.Link>
             <Nav.Link> <Link to="/menu" className='mainmenu'>Menu</Link></Nav.Link>
             <Nav.Link> <Link to="/about" className='mainmenu'>About</Link></Nav.Link>
             <Nav.Link> <Link to="/contact" className='mainmenu'>Contact</Link></Nav.Link>
